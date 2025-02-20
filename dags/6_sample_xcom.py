43d2a2d0048f2e26ffb1177fbe32afaf6258898e
devbrunorm <devbrunorm@gmail.com>
2025-02-19 21:22:36 -0300

    fix dag 6

--- dags/6_sample_xcom.py
+++ dags/6_sample_xcom.py
@@ -1,15 +1,16 @@
 from airflow.models.dag import DAG
 from airflow.operators.python import PythonOperator
+from airflow.operators.dummy import DummyOperator
 from datetime import datetime, timedelta
 
 def random_number(ti):
     from random import randint
-    number = randint(10)
+    number = randint(1, 10)
     print(f"O número escolhido foi: {number}")
     ti.xcom_push(key='number', value=number)
 
 def sum_numbers(ti):
-    numbers = ti.xcom_pull(key='number', task_ids=[f'generate_random_number_{i}' for i range(2)])
+    numbers = ti.xcom_pull(key='number', task_ids=[f'generate_random_number_{i}' for i in range(2)])
     print(f"A soma dos números é de {sum(numbers)}")
 
 with DAG(
@@ -29,13 +30,16 @@ with DAG(
     tags=["example"],
 ) as dag:
     
+    task_start = DummyOperator(task_id = "start")
+    
     print_sum = PythonOperator(
         task_id="sum_numbers",
         python_callable=sum_numbers
     )
 
     for i in range(2):
-        random_number = PythonOperator(
+        gen_random_number = PythonOperator(
             task_id=f"generate_random_number_{i}",
             python_callable=random_number
         )
+        task_start >> gen_random_number >> print_sum
